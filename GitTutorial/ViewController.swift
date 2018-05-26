0ca01a92607130cd682a0ed3827d855ba1312e2f
tomcruis bui <vanhung121115@gmail.com>
2018-05-26 14:37:11 +0700

    commit to branch develop

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Git Tutorial")
+        print("Hi there, this is branch develop")
     }
 
     override func didReceiveMemoryWarning() {
