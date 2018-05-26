3d9a1b2bdb9ce1084e8348ddfec7992a15e074f9
tomcruis bui <vanhung121115@gmail.com>
2018-05-26 15:39:21 +0700

    commit to remote

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         
         print("Git Tutorial")
         print("Hi there, this is branch develop")
+        print("Hello, this is commit to remote")
     }
 
     override func didReceiveMemoryWarning() {
